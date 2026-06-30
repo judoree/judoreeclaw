d5d0fecdda6fe1ae1a948fd649eb9653e661cfb9
김주현 <judoree9629@gmail.com>
2026-06-30 19:40:35 +0900

    Durable sql

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -1,6 +1,24 @@
 import { DurableObject } from 'cloudflare:workers';
 
 export class DurablePotato extends DurableObject<Env> {
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+
+		ctx.storage.sql.exec(`
+			CREATE TABLE IF NOT EXISTS pongs (
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				total INTEGER
+				);
+			`);
+
+		ctx.storage.sql.exec(`
+			INSERT OR IGNORE INTO
+				pongs (id, TOTAL)
+			VALUES
+				(1, 0);
+			`);
+	}
+
 	count = 0;
 	increase() {
 		this.count++;
--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -4,7 +4,7 @@ export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname } = new URL(request.url);
 		if (pathname === '/') {
-			const dp = env.DP.getByName('default');
+			const dp = env.DP.getByName('peter');
 			return new Response(await dp.increase());
 		}
 		return new Response(null, {
