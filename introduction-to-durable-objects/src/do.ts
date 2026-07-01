63a3d34dfb9c7d624eb0ee30855a6f70edb97070
김주현 <judoree9629@gmail.com>
2026-07-01 19:33:14 +0900

    Alarms

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -24,8 +24,19 @@ export class DurablePotato extends DurableObject<Env> {
 
 	count = 0;
 	increase() {
-		const { total } = this.sql.exec(`SELECT total FROM pongs`).one() as { total: number };
-		this.sql.exec(`UPDATE pongs SET total = ? WHERE id = 1`, total + 1);
+		const { total } = this.sql.exec(`UPDATE pongs SET total = total + 1 WHERE id = 1 RETURNING total;`).one() as { total: number };
+		if (total >= 30) {
+			const currentAlarm = this.ctx.storage.getAlarm();
+			console.log;
+			if (currentAlarm === null) {
+				this.ctx.storage.setAlarm(Date.now() + 10_000);
+			}
+		}
 		return `count is ${total + 1}`;
+	}
+	alarm() {
+		this.sql.exec('UPDATE pongs SET total = 0 WHERE id = 1');
+		// search in your `alarms` table and find the next alarm
+		// schedule the next alarm
 	}
 }
