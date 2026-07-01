c80b16250f8b774a89b808be435e94a648ba3f03
김주현 <judoree9629@gmail.com>
2026-07-01 17:58:43 +0900

    Data Explorer

--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -2,9 +2,10 @@ export { DurablePotato } from './do';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const { pathname } = new URL(request.url);
+		const { pathname, searchParams } = new URL(request.url);
+		const nickname = searchParams.get('nickname') ?? 'anon';
 		if (pathname === '/') {
-			const dp = env.DP.getByName('peter');
+			const dp = env.DP.getByName(nickname);
 			return new Response(await dp.increase());
 		}
 		return new Response(null, {
