02e96d51dea90438c3a3e1e4d24abe8b6892e921
김주현 <judoree9629@gmail.com>
2026-07-03 16:43:18 +0900

    Upgrades

--- introduction-to-durable-objects/src/do.ts
+++ introduction-to-durable-objects/src/do.ts
@@ -1,7 +1,23 @@
 import { DurableObject } from 'cloudflare:workers';
 
 export class DurablePotato extends DurableObject<Env> {
-	fetch(request: Request): Response | Promise<Response> {
-		return new Response('hello');
+	fetch(request: Request) {
+		const url = new URL(request.url);
+		const nickname = url.searchParams.get('nickname') ?? 'anon';
+		const webSocketPair = new WebSocketPair();
+
+		const [client, server] = Object.values(webSocketPair);
+
+		this.ctx.acceptWebSocket(server);
+
+		return new Response(null, { status: 101, webSocket: client });
+	}
+
+	webSocketMessage(ws: WebSocket, message: string) {
+		console.log(message);
+	}
+
+	webSocketClose(ws: WebSocket) {
+		console.log('someone left');
 	}
 }
--- introduction-to-durable-objects/src/index.ts
+++ introduction-to-durable-objects/src/index.ts
@@ -3,11 +3,13 @@ export { DurablePotato } from './do';
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		const { pathname, searchParams } = new URL(request.url);
-		const roomId = searchParams.get('roomId') ?? 'public';
-		const upgrade = request.headers.get('Upgrade');
-		if (upgrade) {
-			const dp = env.DP.getByName(roomId);
-			return dp.fetch(request);
+		if (pathname === '/ws') {
+			const roomId = searchParams.get('roomId') ?? 'public';
+			const upgrade = request.headers.get('Upgrade');
+			if (upgrade) {
+				const dp = env.DP.getByName(roomId);
+				return dp.fetch(request);
+			}
 		}
 		return new Response(null, {
 			status: 404,
