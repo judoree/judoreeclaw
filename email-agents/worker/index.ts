ee65b3c804527fa20716e197d91abedca931294e
김주현 <judoree9629@gmail.com>
2026-07-15 17:58:16 +0900

    sending email

--- email-agents/worker/index.ts
+++ email-agents/worker/index.ts
@@ -1,13 +1,58 @@
-import { AIChatAgent } from "@cloudflare/ai-chat";
+import { AIChatAgent, type OnChatMessageOptions } from "@cloudflare/ai-chat";
 import { routeAgentEmail, routeAgentRequest } from "agents";
 import { createAddressBasedEmailResolver, type AgentEmail } from "agents/email";
+import {
+  convertToModelMessages,
+  isLoopFinished,
+  streamText,
+  tool,
+  type StreamTextOnFinishCallback,
+  type ToolSet,
+} from "ai";
 import PostalMime from "postal-mime";
+import { createWorkersAI } from "workers-ai-provider";
+import z from "zod";
 
 export class EmailAgent extends AIChatAgent<Env> {
+  async onChatMessage() {
+    const workersAi = createWorkersAI({
+      binding: this.env.AI,
+    });
+    const result = streamText({
+      model: workersAi("@cf/zai-org/glm-4.7-flash"),
+      messages: await convertToModelMessages(this.messages),
+      tools: {
+        sendTranscript: tool({
+          description: "Send the transcript of the conversation to the user",
+          inputSchema: z.object({
+            email: z.string().meta({ description: "The email of the user" }),
+          }),
+          execute: async ({ email }) => {
+            await this.sendEmail({
+              binding: this.env.EMAIL,
+              to: email,
+              from: "youragetn@agent.com",
+              subject: "Transcript",
+              text: JSON.stringify(this.messages),
+            });
+            return { success: true, sentTo: email };
+          },
+        }),
+      },
+      stopWhen: isLoopFinished(),
+    });
+    return result.toUIMessageStreamResponse();
+  }
+
   async onEmail(email: AgentEmail) {
-    const raw = await email.getRaw();
-    const parsed = await PostalMime.parse(raw);
-    console.log(parsed.to, parsed.from, parsed.text);
+    // const raw = await email.getRaw();
+    // const parsed = await PostalMime.parse(raw);
+    await this.replyToEmail(email, {
+      fromName: "EmailAgent",
+      subject: "Im answering you",
+      contentType: "text/plain",
+      body: `Thank you for your email`,
+    });
   }
 }
 
