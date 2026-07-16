131f468ce59cbdcb02abcabb17be2f1c34eb7b8d
김주현 <judoree9629@gmail.com>
2026-07-16 18:39:30 +0900

    UseVoice

--- voice-agents/src/App.tsx
+++ voice-agents/src/App.tsx
@@ -1,5 +1,43 @@
+import { useVoiceAgent } from "@cloudflare/voice/react";
+import { useAgent } from "agents/react";
+
 function App() {
-  return <div></div>;
+  const agent = useAgent({ agent: "VoiceAgent" });
+  const {
+    startCall,
+    endCall,
+    status,
+    toggleMute,
+    isMuted,
+    interimTranscript,
+    transcript,
+  } = useVoiceAgent({
+    agent: "VoiceAgent",
+  });
+  return (
+    <div>
+      <h1>Talk to Agent</h1>
+      <hr />
+      <h3>{status}</h3>
+      <hr />
+      <h2>interim: {interimTranscript}</h2>
+      <hr />
+      transcript:
+      <ul>
+        {transcript.map((message) => (
+          <li key={message.timestamp}>
+            <strong>{message.role}</strong>: {message.text}
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <button onClick={status === "idle" ? startCall : endCall}>
+        {status === "idle" ? "click to talk" : "click to hang up"}
+      </button>
+      <br />
+      <button onClick={toggleMute}>{isMuted ? "unmute" : "mute"}</button>
+    </div>
+  );
 }
 
 export default App;
