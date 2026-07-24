5b5c756085f0dc6171d0fd7caae7953c50696d78
김주현 <judoree9629@gmail.com>
2026-07-24 16:34:07 +0900

    ToMarkDown

--- rag-agents/worker/index.ts
+++ rag-agents/worker/index.ts
@@ -1,10 +1,33 @@
 import { AIChatAgent } from "@cloudflare/ai-chat";
-import { routeAgentRequest } from "agents";
+import { getAgentByName, routeAgentRequest } from "agents";
 
-export class RAGAgent extends AIChatAgent<Env> {}
+export class RAGAgent extends AIChatAgent<Env> {
+  async ingestPdf(buffer: ArrayBuffer, fileName: string, fileType: string) {
+    const result = await this.env.AI.toMarkdown({
+      name: fileName,
+      blob: new Blob([buffer], { type: fileType }),
+    });
+    console.log(result);
+  }
+}
 
 export default {
-  fetch(request, env) {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/api/upload") {
+      const formData = await request.formData();
+      const file = formData.get("file") as File;
+      const buffer = await file.arrayBuffer();
+      const fileName = `${Date.now()}-${file.name}`;
+      await env.FILES.put(fileName, buffer, {
+        httpMetadata: {
+          contentType: file.type,
+        },
+      });
+      const stub = await getAgentByName(env.RAGAgent, "default");
+      await stub.ingestPdf(buffer, fileName, file.type);
+      return new Response("ok");
+    }
     return (
       routeAgentRequest(request, env) ?? new Response(null, { status: 404 })
     );
