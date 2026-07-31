ce5ac6ae1b8f024e4fe227587e3bf9ef73d5d945
김주현 <judoree9629@gmail.com>
2026-07-31 16:32:42 +0900

    Embeddings

--- rag-agents/worker/index.ts
+++ rag-agents/worker/index.ts
@@ -1,13 +1,31 @@
 import { AIChatAgent } from "@cloudflare/ai-chat";
 import { getAgentByName, routeAgentRequest } from "agents";
+import { embedMany } from "ai";
+import { createWorkersAI } from "workers-ai-provider";
 
 export class RAGAgent extends AIChatAgent<Env> {
-  async ingestPdf(buffer: ArrayBuffer, fileName: string, fileType: string) {
+  async convert(fileName: string, buffer: ArrayBuffer, fileType: string) {
     const result = await this.env.AI.toMarkdown({
       name: fileName,
       blob: new Blob([buffer], { type: fileType }),
     });
-    console.log(result);
+    if (result.format === "error") throw new Error("Could not convert");
+    return result.data;
+  }
+
+  async embedChunks(chunks: string[]) {
+    const workersAi = createWorkersAI({ binding: this.env.AI });
+    const { embeddings } = await embedMany({
+      model: workersAi.textEmbeddingModel("@cf/baai/bge-base-en-v1.5"),
+      values: chunks,
+    });
+    console.log(embeddings[0].length);
+  }
+
+  async ingestPdf(buffer: ArrayBuffer, fileName: string, fileType: string) {
+    const markdown = await this.convert(fileName, buffer, fileType);
+    const chunks = markdown.split("\n\n\n");
+    const embeddings = await this.embedChunks(chunks);
   }
 }
 
