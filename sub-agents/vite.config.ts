e4c7a2a5fa237c582f16ed42ad3377057ae1e3d8
김주현 <judoree9629@gmail.com>
2026-08-04 19:05:20 +0900

    Structured Outputs

--- sub-agents/vite.config.ts
+++ sub-agents/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
 import agents from "agents/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare(), tailwindcss(), agents()],
+  plugins: [agents(), react(), tailwindcss(), cloudflare()],
 });
--- sub-agents/worker/index.ts
+++ sub-agents/worker/index.ts
@@ -1,12 +1,55 @@
-export default {
-  fetch(request) {
-    const url = new URL(request.url);
+import { AIChatAgent } from "@cloudflare/ai-chat";
+import { callable, routeAgentRequest } from "agents";
+import { generateText, Output } from "ai";
+import { createWorkersAI } from "workers-ai-provider";
+import z from "zod";
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
-    }
-    return new Response(null, { status: 404 });
+export type OrchestratorState = {
+  status: "idle" | "planning";
+};
+
+export class Orchestrator extends AIChatAgent<Env, OrchestratorState> {
+  initialState: OrchestratorState = {
+    status: "idle",
+  };
+
+  @callable()
+  async research(query: string) {
+    this.setState({
+      status: "planning",
+    });
+    const workersAi = createWorkersAI({ binding: this.env.AI });
+    const {
+      output: { queries },
+    } = await generateText({
+      model: workersAi("@cf/zai-org/glm-4.7-flash"),
+      output: Output.object({
+        schema: z.object({
+          queries: z
+            .array(
+              z.string().meta({
+                description:
+                  "A query for a search engine, exploring an angle of research",
+              })
+            )
+            .min(3)
+            .max(3)
+            .meta({
+              description:
+                "Three distinct research angles. Phrased as search queries",
+            }),
+        }),
+      }),
+      prompt: `Break this topic into 3 different research angles: ${query}\nEach has to be phrased as a researc query`,
+    });
+    console.log(queries);
+  }
+}
+
+export default {
+  fetch(request, env) {
+    return (
+      routeAgentRequest(request, env) ?? new Response(null, { status: 404 })
+    );
   },
 } satisfies ExportedHandler<Env>;
